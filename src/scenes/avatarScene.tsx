d963901830a39c7388805b8ad51c41eef190030a
Gabriel Vargas <gabrielfvf@outlook.com>
2019-11-25 17:43:22 -0300

    Add firebase

--- src/scenes/avatarScene.tsx
+++ src/scenes/avatarScene.tsx
@@ -3,31 +3,42 @@ import { StyleSheet, View } from 'react-native';
 import { Input, Button, Avatar } from 'react-native-elements';
 
 export default function avatarScene() {
-    const { container, input } = styles;
+	const { container, input } = styles;
 
-    return (
-        <View style={container}>
-            <Avatar rounded size="xlarge" />
-            <Input inputContainerStyle={input} placeholderTextColor={'white'} inputStyle={{ color: 'white' }} placeholder={'Nome'} />
-            <Input inputContainerStyle={input} placeholderTextColor={'white'} inputStyle={{ color: 'white' }} placeholder={'Sobrenome'} /> 
-        </View>
-    )
+	return (
+		<View style={container}>
+			<Avatar rounded size="xlarge" />
+			<Input
+				inputContainerStyle={input}
+				placeholderTextColor={'white'}
+				inputStyle={{ color: 'white' }}
+				placeholder={'Nome'}
+			/>
+			<Input
+				inputContainerStyle={input}
+				placeholderTextColor={'white'}
+				inputStyle={{ color: 'white' }}
+				placeholder={'Sobrenome'}
+			/>
+			<Button title={'Próximo'} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      input: {
-          paddingLeft: 5,
-          marginTop: 20,
-          marginHorizontal: 30, 
-          borderColor: 'black',
-          backgroundColor: '#79C7FF',
-          borderRadius: 8,  
-          borderWidth: 2
-    }
-})
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	input: {
+		paddingLeft: 5,
+		marginTop: 20,
+		marginHorizontal: 30,
+		borderColor: 'black',
+		backgroundColor: '#79C7FF',
+		borderRadius: 8,
+		borderWidth: 2
+	}
+});
---
+++ src/scenes/privacyScene.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Input, Button, Avatar } from 'react-native-elements';
+
+export default function privacyScene() {}
+
+const styles = Stylesheet.create({});
