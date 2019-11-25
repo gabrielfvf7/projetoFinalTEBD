7cec1fd6f1bdb7430c37a12140097ec82827718f
Gabriel Vargas <gabrielfvf@outlook.com>
2019-11-25 19:22:17 -0300

    Colocando botões e conexao com firebase

--- Router.tsx
+++ Router.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Router, Scene, Stack } from 'react-native-router-flux';
 import welcomeScene from './src/scenes/welcomeScene';
 import avatarScene from './src/scenes/avatarScene';
+import privacyScene from './src/scenes/privacyScene';
 
 const AppRouter = () => (
-        <Router>
-            <Stack key="root">
-                <Scene key="welcome" component={welcomeScene} initial={true} hideNavBar={true} />
-                <Scene key="avatar" component={avatarScene} hideNavBar={true} />
-            </Stack>
-        </Router>
+    <Router>
+        <Stack key="root">
+            <Scene key="welcome" component={welcomeScene} initial={true} hideNavBar={true} />
+            <Scene key="avatar" component={avatarScene} hideNavBar={true} />
+            <Scene key="privacy" component={privacyScene} hideNavBar={true} />
+        </Stack>
+    </Router>
 )
 
 export default AppRouter;
--- src/scenes/privacyScene.tsx
+++ src/scenes/privacyScene.tsx
@@ -1,30 +1,81 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Input, Button, Avatar } from 'react-native-elements';
-import * as firebase from firebase;
 
-interface privacySceneProps {
-    
+const firebase = require('firebase');
+require('firebase/firestore');
+
+interface privacySceneProps {}
+
+interface privacySceneState {
+	nome: boolean;
+	sobrenome: boolean;
 }
 
-export class privacyScene extends Component<privacySceneProps> {
+export default class privacyScene extends Component<
+	privacySceneProps,
+	privacySceneState
+> {
+	public state: privacySceneState = {
+		nome: false,
+		sobrenome: false
+	};
 
-    componentWillMount() {
+	public componentDidMount() {
+		firebase.initializeApp(firebaseConfig);
+		this.preencherCampos();
+	}
 
-    }
+	preencherCampos() {
+		const firestore = firebase.firestore();
+		const ref = firestore.collection('users');
+		const query = ref.where('id_user', '==', 0);
+		let user: any;
 
-	return (
-		<View>
-			<Button />
-		</View>
-	);
+		query.get().then(snapshot => {
+			snapshot.forEach(doc => {
+				user = doc.data();
+				console.log(user);
+				this.setState({
+					nome: user.dados_dele.nome,
+					sobrenome: user.dados_dele.sobrenome
+				});
+			});
+		});
+	}
+
+	public render() {
+		const { button, buttonDisabled, container } = styles;
+
+		return (
+			<View style={container}>
+				<Button
+					buttonStyle={!this.state.nome ? button : buttonDisabled}
+					title={'Nome'}
+					onPress={() => this.setState({ nome: !this.state.nome })}
+				/>
+				<Button
+					buttonStyle={button}
+					title={'Sobrenome'}
+					disabled={!this.state.sobrenome}
+					onPress={() =>
+						this.setState({ sobrenome: !this.state.sobrenome })
+					}
+				/>
+			</View>
+		);
+	}
 }
 
 const firebaseConfig = {
-	apiKey: '<YOUR-API-KEY>',
-	authDomain: '<YOUR-AUTH-DOMAIN>',
-	databaseURL: '<YOUR-DATABASE-URL>',
-	storageBucket: '<YOUR-STORAGE-BUCKET>'
+	apiKey: 'AIzaSyCVQBEUIQvk1YCcbm01frGjY0tRVuNutTY',
+	authDomain: 'trabalhotebd.firebaseapp.com',
+	databaseURL: 'https://trabalhotebd.firebaseio.com',
+	projectId: 'trabalhotebd',
+	storageBucket: 'trabalhotebd.appspot.com',
+	messagingSenderId: '693631680483',
+	appId: '1:693631680483:web:122db7fbb8dc5fa83aead6',
+	measurementId: 'G-6G9XP23KMP'
 };
 
 const styles = StyleSheet.create({
@@ -33,5 +84,12 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center'
+	},
+	button: {
+		borderRadius: 8,
+		paddingHorizontal: 25
+	},
+	buttonDisabled: {
+		backgroundColor: 'gray'
 	}
 });
